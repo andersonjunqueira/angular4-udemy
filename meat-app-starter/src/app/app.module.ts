52d245df459f0cdb9539083c2409fe554f7e0563
anderson.osorio <anderson.osorio@mirante.net.br>
2017-11-21 08:43:30 -0200

    preloading modules

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { PreloadAllModules, RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ROUTES } from './app.routes';
@@ -38,8 +38,8 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
     HttpModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(ROUTES),
-    SharedModule.forRoot()
+    SharedModule.forRoot(),
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules })
   ],
   providers: [{ provide: LOCALE_ID, useValue:'pt-BR'}],
   bootstrap: [AppComponent]

f82164ec1963fdfe3d0be33f89895a306e549603
anderson.osorio <anderson.osorio@mirante.net.br>
2017-11-21 07:51:37 -0200

    about module

--- meat-app-starter/src/app/app.routes.ts
+++ meat-app-starter/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailsComponent } from './restaurant-details/restaurant-details.component';
 import { MenuComponent } from './restaurant-details/menu/menu.component';
@@ -10,9 +9,8 @@ import { OrderComponent } from './order/order.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 export const ROUTES: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'about', component: AboutComponent},
-  { path: 'restaurants', component: RestaurantsComponent},
+  { path: '', component: HomeComponent },
+  { path: 'restaurants', component: RestaurantsComponent },
   { path: 'restaurants/:id', component: RestaurantDetailsComponent,
     children: [
       { path: '', redirectTo: 'menu', pathMatch: 'full' },
@@ -20,6 +18,7 @@ export const ROUTES: Routes = [
       { path: 'reviews', component: ReviewsComponent },
     ]
   },
-  { path: 'order', component: OrderComponent},
-  { path: 'order-summary/:id', component: OrderSummaryComponent},
+  { path: 'order', component: OrderComponent },
+  { path: 'order-summary/:id', component: OrderSummaryComponent },
+  { path: 'about', loadChildren: './about/about.module#AboutModule' }
 ];
